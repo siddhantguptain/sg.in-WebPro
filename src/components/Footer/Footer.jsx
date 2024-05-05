98937eeaeab2c65a13384a77b7dfd40ecd9aeecb
Siddhant Gupta <me@siddhantgupta.in>
2024-05-06 02:09:00 +0530

    Added the Footer  Code FIXED

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import style from "./Footer.module.css";
+
+const Footer = () =>{
+    return(
+        <>
+            <div className={style.footerContainer}>
+                <div className={style.footerContainerCodeNIL}>{`</>`}</div>
+                <div className={style.footerContainerCodeNIL}>{`); }`}</div>
+            </div>
+        </>
+    );
+}
+export default Footer;
