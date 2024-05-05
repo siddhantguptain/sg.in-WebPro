bf0466916a97935b7fd99fd9415358dfc1348852
Siddhant Gupta <me@siddhantgupta.in>
2024-05-06 02:09:27 +0530

    Added the Temp side Bar  FIXED

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,13 @@
+import style from "./SideBar.module.css";
+
+const SideBar = () => {
+    return(
+            <>
+                <div className={style.sideBarContainer}>
+                     SideBar
+                </div>
+                
+            </>
+    );
+}
+export default SideBar;
